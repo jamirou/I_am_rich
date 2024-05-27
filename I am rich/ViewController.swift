869de122d698251be042bc2435d67236b97fb2f8
jamirou <devjamiro@gmail.com>
2024-05-27 19:49:45 -0400

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Jamiro Manriquez Aguilera on 27-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
